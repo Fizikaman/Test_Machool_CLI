bdb022fb272cf79fb9ece2d087c545b09c2a9f88
Fizikaman <fizikaman26@gmail.com>
2024-05-30 09:35:10 +0300

    config

---
+++ config.py
@@ -0,0 +1,6 @@
+API_BASE_URL = 'https://api.sandbox.machool.com'
+HEADERS = {
+    'x-key-id': 'bCahFZHQc3',
+    'x-api-key': '645744e8-a047-421a-b882-7d2471f6bab5',
+    'Content-Type': 'application/json'
+}
